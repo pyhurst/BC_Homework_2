d124582e75000c4ab09ce86eb6d49b72e05a38ba
Philip Hurst <youngphil9@gmail.com>
2020-05-13 07:24:59 -0500

    all contact fields save to mysql database, appends successful message if saved or error if not all of the fields have a value

--- public/js/contact.js
+++ public/js/contact.js
@@ -5,21 +5,41 @@ $(function () {
     const phoneNum = $("#phone-number-input");
     const category = $("#category-dropdown");
     const details = $("#details-input");
+    const display = $("#submit-message");
 
 
     $("#contact-submit").on("click", function (e) {
         e.preventDefault();
-        // console.log(firstName.val());
 
         const userInfo = {
-            firstName: firstName.val().trim()
+            firstName: firstName.val().trim(),
+            lastName: lastName.val().trim(),
+            email: email.val().trim(),
+            phone: phoneNum.val().trim(),
+            type: category.val(),
+            details: details.val().trim()
         }
 
+        if (firstName.val() === "" || lastName.val() === "" || email.val() === "" || phoneNum.val() === "" || details.val() === ""){
+            display.empty();
+            display.append($("<p>").attr("style", "color: red;").text("Oops! All fields must have a value!"));
+            setTimeout(function() {
+                display.empty();
+            }, 5000);
+        } else {
         $.ajax("/api/contact", {
             type: "POST",
             data: userInfo
         }).then(function (data) {
             console.log("saved");
+            display.empty();
+            display.append($("<p>").attr("style", "color: green;").text("Successfully submitted!"));
+            setTimeout(function() {
+                display.empty();
+            }, 5000);
         });
+
+        $(".clear").val("");
+    }
     });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,12 @@ module.exports = function(app) {
     app.post("/api/contact", function(req, res) {
         // console.log(req.body)
         db.Contact.create({
-            firstName: req.body.firstName
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            type: req.body.type,
+            details: req.body.details
         }).then(function(data) {
             res.json(data);
         });

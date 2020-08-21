54ac84d8c59e23a489e4a4bb47151691c0d130ff
Philip Hurst <youngphil9@gmail.com>
2020-08-21 11:58:14 -0500

    added resume pdf, resume html css

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,4 +16,8 @@ module.exports = function(app) {
       app.get("/portfolio", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/portfolio.html"));
       });
+
+      app.get("/resume", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/resume.html"));
+      });
 };

53c432fabdeba5b1ced63bb908b1695d175a60e8
Philip Hurst <youngphil9@gmail.com>
2021-03-05 11:57:09 -0600

    changed to single html page, scroll reveal added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,19 +5,19 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
       });
     
-      app.get("/about", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/about.html"));
-      });
+      // app.get("/about", function(req, res) {
+      //   res.sendFile(path.join(__dirname, "../public/about.html"));
+      // });
     
-      app.get("/contact", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/contact.html"));
-      });
+      // app.get("/contact", function(req, res) {
+      //   res.sendFile(path.join(__dirname, "../public/contact.html"));
+      // });
     
-      app.get("/portfolio", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/portfolio.html"));
-      });
+      // app.get("/portfolio", function(req, res) {
+      //   res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+      // });
 
-      app.get("/resume", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/resume.html"));
-      });
+      // app.get("/resume", function(req, res) {
+      //   res.sendFile(path.join(__dirname, "../public/resume.html"));
+      // });
 };
